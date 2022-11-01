df3542ea9db90d02feee2ee21bf61cd64aa7e365
Sergio Miguez <s.miguez.aparicio@gmail.com>
2022-11-01 20:45:56 +0000

    Comments update_script

--- update_repo.sh
+++ update_repo.sh
@@ -5,7 +5,7 @@ SOURCE=/home/$D/$D/
 DEST_VM=work
 DESTINATION=/home/user/unikernel-$D/
 
-cd 
+
 cd $SOURCE
 
 # SIMPLE SYNC
@@ -14,12 +14,18 @@ cd $SOURCE
 #done
 
 
+# Create a Zipfile
 zipfile=send.zip
 zip -r $zipfile .
-cat $zipfile | qvm-run --pass-io $DEST_VM 'cat > '"$DESTINATION$zipfile"''
-qvm-run --pass-io $DEST_VM "cd $DESTINATION; unzip $zipfile; rm $zipfile"
-rm -r $zipfile
 
+# Sending Zipfile to destination VM
+cat $zipfile | qvm-run --pass-io $DEST_VM 'cat > '"$DESTINATION$zipfile"''
+
+# Running commands in the destination VM to unzip and clean the folder
+qvm-run --pass-io $DEST_VM "cd $DESTINATION; unzip $zipfile; rm $zipfile"
+
+# Removing the Zipfile
+rm -r $zipfile
 
 echo "DONE"
 
