01abb0889fcc28304e450db5f0c7a40bc0624759
Sergio Miguez <s.miguez.aparicio@gmail.com>
2023-04-12 12:29:47 +0100

    Dissertation and results

--- Custom Kernel Development/Testing Performance/Results/count_number_of_features.py
+++ Custom Kernel Development/Testing Performance/Results/count_number_of_features.py
@@ -11,7 +11,23 @@ if os.path.isfile(filename):  # Check if file exists
             if not line.startswith("#") and line.strip():
                 count += 1
 
-    print("Number of non-empty and non-commented lines in the file:", count)
+    print(".config_v50_final number of features:", count)
+    
+else:
+    print(f"Error: {filename} does not exist.")
+    
+
+filename = "..\\..\\.defconfig_x86"  # Relative path to file
+if os.path.isfile(filename):  # Check if file exists
+    count = 0
+
+    with open(filename, 'r') as file:
+        for line in file:
+            # Check if line starts with "#" or is empty
+            if not line.startswith("#") and line.strip():
+                count += 1
+
+    print(".defconfig_x86 number of features:", count)
     
 else:
     print(f"Error: {filename} does not exist.")
